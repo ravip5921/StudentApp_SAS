01b61ad67b5b06f0d1ba02de48a80b57291dc27c
Ravi <075bct065.ravi@pcampus.edu.np>
2022-03-16 18:57:01 +0545

    UI Improved.

---
+++ float.py
@@ -0,0 +1,5 @@
+def conv(r, g, b):
+    return [r/256, g/256, b/256]
+
+
+print(conv(84, 81, 214))
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from glob import escape
 import server.client_student
 
 
@@ -6,14 +5,17 @@ import cv2
 import face_recognition
 import os
 
+
 from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivy.uix.camera import Camera
 from kivy.uix.image import Image
-from kivymd.uix.boxlayout import BoxLayout
-from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDFloatingActionButton
 from kivymd.uix.label import MDLabel
 from kivymd.uix.textfield import MDTextField
+# from kivymd.uix.behaviors.backgroundcolor_behavior import BackgroundColorBehavior
+
 Window.top = 50
 Window.size = (540, 960)
 filePresent = True
@@ -27,121 +29,153 @@ except:
 
 class StudentApp(MDApp):
     rollNoSaved = ""
+    pictureRemoved = False
 
     def build(self):
         # app LOGO
-        # self.imageObj = Image(source="./assets/logo.png")
-        # # layout for image of logo
-        # self.imageLayout = BoxLayout(size_hint=(0.2, 0.2),
-        #                              pos_hint={'x': 0.05})
-        # self.imageLayout.add_widget(self.imageObj)
+        self.theme_cls.theme_style = "Light"
+        self.imageObj = Image(source="./assets/icon.png")
+        # layout for image of logo
+        imageLayout = MDBoxLayout(size_hint=(0.35, 0.35),
+                                  pos_hint={'x': 0.0, 'y': 0.2})
+        imageLayout.add_widget(self.imageObj)
         # widgets for input
-        # self.rollNoL = MDLabel(text="Roll No:")
-        # self.AttendanceCodeL = MDLabel(text="Attendance Code: ")
+        self.rollNoL = MDLabel(text="  Roll No:", theme_text_color="Custom")
+        self.rollNoL.text_color = [0, 1, 0, 1]
+        self.AttendanceCodeL = MDLabel(
+            text="  Attendance Code:", theme_text_color="Custom")
+        self.AttendanceCodeL.text_color = [0, 1, 0, 1]
 
-        # self.rollNoL = MDLabel(text=" ",
-        #                        size_hint=(0.9, 0.1))
-        # self.AttendanceCodeL = MDLabel(text=" ",
-        #                                size_hint=(0.9, 0.25),
-        #                                pos_hint={'center_x': 0.5, 'center_y': 0.25})
+        self.rollNoT = MDTextField(hint_text="Enter Roll no :")
+        self.rollNoT.color_mode = "custom"
+        # self.rollNoT.fill_color = [0.0117, 0.96875, 0.984375, 1]
+        self.rollNoT.line_color_focus = [0.01171875, 0.515625, 0.984375, 1]
+        self.rollNoT.line_color_normal = [0.984375, 0.53125, 0.0117, 1]
+        # self.rollNoT.mode = "fill"
 
-        self.rollNoT = MDTextField(hint_text="Enter Roll no :")  # ,
-        # size_hint=(0.9, 0.5),
-        # pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        # self.AttendanceCodeT = MDTextField(
-        #     hint_text="Enter Attendance Code : ",
-        #     size_hint=(0.9, 0.25),
-        #     pos_hint={'center_x': 0.5, 'center_y': 0.5}
-        # )
+        self.AttendanceCodeT = MDTextField(
+            hint_text="Enter Attendance Code : ")
+        self.AttendanceCodeT.color_mode = "custom"
+        self.AttendanceCodeT.line_color_focus = [
+            0.01171875, 0.515625, 0.984375, 1]
+        self.AttendanceCodeT.line_color_normal = [0.984375, 0.53125, 0.0117, 1]
 
-        inputLayout = BoxLayout(orientation="vertical")
-        # size_hint=(0.8, 0.8),
-        # pos_hint={'center_x': 0.5, 'center_y': 0.5}
-        # )
+        inputLayout = MDBoxLayout(orientation="vertical",
+
+                                  pos_hint={'x': 0.1, 'y': 0.1}
+                                  )
 
         # setting text of roll no from saved file
-        # if filePresent:
-        #     self.rollNoT.text = fileP.readline()
+        if filePresent:
+            self.rollNoT.text = fileP.readline()
+            fileP.close()
 
+        inputLayout.size_hint = (0.8, 0.5)
+        inputLayout.md_bg_color = [0.328125, 0.31640625, 0.8359375, 1]
         # Adding widgets for input to input layout
-        # inputLayout.add_widget(self.rollNoL)
+        inputLayout.add_widget(MDLabel())
+        inputLayout.add_widget(self.rollNoL)
         inputLayout.add_widget(self.rollNoT)
-        # inputLayout.add_widget(self.AttendanceCodeL)
-        # inputLayout.add_widget(self.AttendanceCodeT)
+        inputLayout.add_widget(MDLabel())
+        inputLayout.add_widget(self.AttendanceCodeL)
+        inputLayout.add_widget(self.AttendanceCodeT)
+        inputLayout.add_widget(MDLabel())
 
         # Message label
-        # self.messageL = MDLabel(text="Authenticate your face:")
-        # # Layout for Message
-        # messageLayout = BoxLayout(orientation="vertical",
-        #                           size_hint=(1, 1),
-        #                           pos_hint={'center_x': 0.5, 'center_y': 0.5}
-        #                           )
+        self.messageL = MDLabel(
+            text="Authenticate your face:", theme_text_color="Custom")
+        self.messageL.text_color = [0, 1, 0, 1]
+        # Layout for Message
+        messageLayout = MDBoxLayout(orientation="vertical",
+                                    size_hint=(0.8, 0.2),
+                                    pos_hint={'x': 0.1, 'y': 0.1}
+                                    )
 
-        # messageLayout.add_widget(self.messageL)
+        messageLayout.add_widget(self.messageL)
 
-        # cameraLayout = BoxLayout(size_hint=(0.9, 0.4),
-        #                          pos_hint={'x': 0.05, 'y': 0.2}
-        #                          )
         # Camera object
-        # self.cameraObj = Camera()
-        # self.cameraObj.resolution = (480, 480)
-        # cameraLayout.add_widget(self.cameraObj)
-        # # Capture Button
-        # self.buttonObj = MDRaisedButton(text="Capture")
-        # self.buttonObj.size_hint = (.2, .10)
-        # self.buttonObj.pos_hint = {'x': .40, 'y': .2}
-        # self.buttonObj.bind(on_press=self.takeImage)
+        self.cameraObj = Camera()
+        self.cameraObj.resolution = (640, 480)
+        cameraLayout = MDBoxLayout(size_hint=(0.8, 1),
+                                   pos_hint={'x': 0.1})
+        cameraLayout.add_widget(self.cameraObj)
+        # Capture Button
+        self.buttonObj = MDFloatingActionButton(icon='camera')
+        # self.buttonObj.size_hint = (.2, .2)
+        self.buttonObj.pos_hint = {'x': .45, 'bottom': 0.1}
+        self.buttonObj.bind(on_press=self.takeImage)
         # layout for camera and capture button
-        myBoxLayout = BoxLayout(orientation="vertical")
-        # myBoxLayout.add_widget(self.imageLayout)
-        # myBoxLayout.add_widget(inputLayout)
-        # myBoxLayout.add_widget(messageLayout)
-        # myBoxLayout.add_widget(cameraLayout)
-        # myBoxLayout.add_widget(self.buttonObj)
+        myBoxLayout = MDBoxLayout(
+            orientation="vertical",
+            md_bg_color=[0.390625, 0.32421875, 0.83203125, 1]
+            # md_bg_color=[0.796875, 1, 1, 1]
+        )
 
-        return inputLayout
+        myBoxLayout.add_widget(imageLayout)
+        myBoxLayout.add_widget(MDLabel(text=" ", size_hint=(1, 0.2)))
+        myBoxLayout.add_widget(inputLayout)
+        myBoxLayout.add_widget(messageLayout)
+        myBoxLayout.add_widget(cameraLayout)
+        myBoxLayout.add_widget(self.buttonObj)
+        myBoxLayout.add_widget(MDLabel(text=" ", size_hint=(1, 0.1)))
 
-    # def takeImage(self, *args):
-    #     self.filename = str(self.rollNoT.text + "+" +
-    #                         self.AttendanceCodeT.text+".png")
-    #     self.rollNo = str(self.rollNoT.text)
-    #     self.acode = self.AttendanceCodeT.text
-    #     print(self.filename)
+        return myBoxLayout
 
-    #     try:
-    #         self.cameraObj.export_to_png(self.filename)
-    #         imag = face_recognition.load_image_file(self.filename)
-    #         imag = cv2.cvtColor(imag, cv2.COLOR_BGR2RGB)
-    #         self.encodingsData = face_recognition.face_encodings(imag)
-    #         if len(self.encodingsData) > 0:
-    #             self.encodingsData = self.encodingsData[0]
-    #             self.encodingsData = self.encodingsData.tolist()
+    def takeImage(self, *args):
+        self.filename = str(self.rollNoT.text + "+" +
+                            self.AttendanceCodeT.text+".png")
+        self.rollNo = str(self.rollNoT.text)
+        self.acode = self.AttendanceCodeT.text
+        print(self.filename)
 
-    #             dataFromServer = server.client_student.markAttendance(
-    #                 self.rollNo, int(self.acode), self.encodingsData)
-    #             # print(dataFromServer)
-    #             if "error" in dataFromServer:
-    #                 print(dataFromServer["error"])
-    #                 self.messageL.text = str(dataFromServer["error"])
-    #             else:
-    #                 print(dataFromServer["success"])
-    #                 self.messageL.text = str(dataFromServer["success"])
-    #             os.remove(self.filename)
-    #             pictureRemoved = True
-    #             if filePresent:
-    #                 os.remove("./userData.txt")
+        try:
+            self.cameraObj.export_to_png(self.filename)
+            imag = face_recognition.load_image_file(self.filename)
+            imag = cv2.cvtColor(imag, cv2.COLOR_BGR2RGB)
+            self.encodingsData = face_recognition.face_encodings(imag)
+            if len(self.encodingsData) > 0:
+                self.encodingsData = self.encodingsData[0]
+                self.encodingsData = self.encodingsData.tolist()
+                # print(self.encodingsData)
+                #  Send embeddings to server
+                print(self.rollNo)
+                dataFromServer = server.client_student.markAttendance(
+                    self.rollNo, int(self.acode), self.encodingsData)
+                print(dataFromServer)
+                if "error" in dataFromServer:
+                    print(dataFromServer["error"])
+                    self.messageL.text = str(dataFromServer["error"])
+                else:
+                    print(dataFromServer["success"])
+                    self.messageL.text = str(dataFromServer["success"])
+                try:
+                    os.remove(self.filename)
+                    self.pictureRemoved = True
+                except Exception as e:
+                    print("Image file remove error: ", e)
 
-    #             fileP = open("./userData.txt", "w")
-    #             fileP.write(self.rollNo)
-    #         else:
-    #             self.messageL.text = "No Face Detected. Try again!"
-    #             os.remove(self.filename)
-    #     except Exception as e:
-    #         # print("No face detected.")
-    #         print("error :", e)
-    #         if not pictureRemoved:
-    #             os.remove(self.filename)
-    #         self.encodingsData = None
+                if filePresent:
+                    try:
+                        os.remove("./userData.txt")
+
+                    except Exception as e:
+                        print("Text file remove error: ", e)
+
+                try:
+                    fileP = open("./userData.txt", "w")
+                    fileP.write(self.rollNo)
+
+                except Exception as e:
+                    print("File error: ", e)
+            else:
+                self.messageL.text = "No face detected. Try Again"
+                os.remove(self.filename)
+        except Exception as e:
+            print("No face detected.")
+            print("error :", e)
+            if not self.pictureRemoved:
+                os.remove(self.filename)
+            self.encodingsData = None
 
 
 if __name__ == "__main__":
